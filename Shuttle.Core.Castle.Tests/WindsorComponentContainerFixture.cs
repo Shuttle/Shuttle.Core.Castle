976d7a9474ed28118f8f3035ae144929102a6ef5
Eben <me@ebenroux.co.za>
2016-11-05 07:35:22 +0200

    - initial commit

---
+++ Shuttle.Core.Castle.Tests/WindsorComponentContainerFixture.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Windsor;
+using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Core.Castle.Tests
+{
+    [TestFixture]
+    public class WindsorComponentContainerFixture
+    {
+        [Test]
+        public void Should_be_able_to_register_and_resolve_a_type()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+            var serviceType = typeof(IDoSomething);
+            var implementationType = typeof(DoSomething);
+            var bogusType = typeof(object);
+
+            container.Register(serviceType, implementationType, Lifestyle.Singleton);
+
+            Assert.NotNull(container.Resolve(serviceType));
+            Assert.AreEqual(implementationType, container.Resolve(serviceType).GetType());
+            Assert.Throws<TypeResolutionException>(() => container.Resolve(bogusType));
+        }
+
+        [Test]
+        public void Should_not_be_able_to_register_duplicate_types()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+            var serviceType = typeof(IDoSomething);
+            var implementationType = typeof(DoSomething);
+
+            container.Register(serviceType, implementationType, Lifestyle.Singleton);
+
+            Assert.Throws<TypeRegistrationException>(() => container.Register(serviceType, implementationType, Lifestyle.Singleton));
+
+            Assert.NotNull(container.Resolve(serviceType));
+
+            Assert.Throws<TypeRegistrationException>(() => container.Register(serviceType, new DoSomething()));
+        }
+
+        [Test]
+        public void Should_not_be_able_to_register_duplicate_instances()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+            var serviceType = typeof(IDoSomething);
+            var implementationType = typeof(DoSomething);
+
+            container.Register(serviceType, new DoSomething());
+
+            Assert.NotNull(container.Resolve(serviceType));
+
+            Assert.Throws<TypeRegistrationException>(() => container.Register(serviceType, implementationType, Lifestyle.Singleton));
+            Assert.Throws<TypeRegistrationException>(() => container.Register(serviceType, new DoSomething()));
+        }
+
+        [Test]
+        public void Should_be_able_to_use_constructor_injection()
+        {
+            var container = new WindsorComponentContainer(new WindsorContainer());
+            var serviceType = typeof(IDoSomething);
+            var implementationType = typeof(DoSomethingWithDependency);
+
+            container.Register(serviceType, implementationType, Lifestyle.Singleton);
+
+            Assert.Throws<TypeResolutionException>(() => container.Resolve(serviceType));
+            Assert.Throws<TypeResolutionException>(() => container.Resolve<IDoSomething>());
+
+            var someDependency = new SomeDependency();
+
+            container.Register(typeof(ISomeDependency), someDependency);
+
+            Assert.AreSame(someDependency, container.Resolve<IDoSomething>().SomeDependency);
+        }
+    }
+}
---
+++ Shuttle.Core.Castle/WindsorComponentContainer.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Handlers;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Core.Castle
+{
+    public class WindsorComponentContainer : IComponentContainer
+    {
+        private readonly IWindsorContainer _container;
+
+        public WindsorComponentContainer(IWindsorContainer container)
+        {
+            Guard.AgainstNull(container, "container");
+
+            _container = container;
+        }
+
+        public object Resolve(Type serviceType)
+        {
+            Guard.AgainstNull(serviceType, "serviceType");
+
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ComponentNotFoundException ex)
+            {
+                throw new TypeResolutionException(ex.Message, ex);
+            }
+            catch (HandlerException ex)
+            {
+                throw new TypeResolutionException(ex.Message, ex);
+            }
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        public IComponentContainer Register(Type serviceType, Type implementationType, Lifestyle lifestyle)
+        {
+            Guard.AgainstNull(serviceType, "serviceType");
+            Guard.AgainstNull(implementationType, "implementationType");
+
+            try
+            {
+                switch (lifestyle)
+                {
+                    case Lifestyle.Thread:
+                        {
+                            _container.Register(
+                                Component.For(serviceType).ImplementedBy(implementationType).LifestylePerThread());
+
+                            break;
+                        }
+                    case Lifestyle.Transient:
+                        {
+                            _container.Register(
+                                Component.For(serviceType).ImplementedBy(implementationType).LifestyleTransient());
+
+                            break;
+                        }
+                    default:
+                        {
+                            _container.Register(
+                                Component.For(serviceType).ImplementedBy(implementationType).LifestyleSingleton());
+
+                            break;
+                        }
+                }
+            }
+            catch (ComponentRegistrationException ex)
+            {
+                throw new TypeRegistrationException(ex.Message, ex);
+            }
+            return this;
+        }
+
+        public IComponentContainer Register(Type serviceType, object instance)
+        {
+            Guard.AgainstNull(serviceType, "serviceType");
+            Guard.AgainstNull(instance, "instance");
+
+            try
+            {
+                _container.Register(Component.For(serviceType).Instance(instance));
+            }
+            catch (ComponentRegistrationException ex)
+            {
+                throw new TypeRegistrationException(ex.Message, ex);
+            }
+
+            return this;
+        }
+    }
+}
